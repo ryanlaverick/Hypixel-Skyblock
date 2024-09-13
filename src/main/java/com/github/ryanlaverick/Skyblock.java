14ae99169cc2107d711933f1d617cd2c8e42fb8a
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2024-09-14 00:05:40 +0100

    inline database manager due to connection being statically available

--- src/main/java/com/github/ryanlaverick/Skyblock.java
+++ src/main/java/com/github/ryanlaverick/Skyblock.java
@@ -4,15 +4,9 @@ import com.github.ryanlaverick.framework.database.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Skyblock extends JavaPlugin {
-    private DatabaseManager databaseManager;
-
     @Override
     public void onEnable() {
-        this.databaseManager = new DatabaseManager(this);
-        this.databaseManager.establishConnection();
-    }
-
-    public DatabaseManager getDatabaseManager() {
-        return databaseManager;
+        DatabaseManager databaseManager = new DatabaseManager(this);
+        databaseManager.establishConnection();
     }
 }
