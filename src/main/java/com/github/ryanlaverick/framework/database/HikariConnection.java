86aeb0dc7f7730fef0d527fb62fe53a8eb0df760
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2024-09-13 21:11:56 +0100

    creates concrete implementation for Hikari/SQLite databases

---
+++ src/main/java/com/github/ryanlaverick/framework/database/HikariConnection.java
@@ -0,0 +1,79 @@
+package com.github.ryanlaverick.framework.database;
+
+import com.github.ryanlaverick.framework.database.exceptions.ConnectionAlreadyOpenException;
+import com.github.ryanlaverick.framework.database.exceptions.ConnectionNotOpenException;
+import com.github.ryanlaverick.framework.database.exceptions.InvalidConnectionCredentialsException;
+import com.github.ryanlaverick.framework.database.exceptions.UnclosableConnectionException;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.SQLException;
+
+public final class HikariConnection implements Connection {
+    private final String host;
+    private final String port;
+    private final String database;
+    private final String username;
+    private final String password;
+    private final int maximumConnections;
+
+    private HikariDataSource hikariDataSource;
+
+    public HikariConnection(String host, String port, String database, String username, String password, int maximumConnections) {
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+        this.maximumConnections = maximumConnections;
+    }
+
+    @Override
+    public void openConnection() throws ConnectionAlreadyOpenException, InvalidConnectionCredentialsException, SQLException {
+        if (this.getConnection() != null) {
+            throw new ConnectionAlreadyOpenException();
+        }
+
+        try (HikariDataSource connection = new HikariDataSource()) {
+            connection.setMaximumPoolSize(this.maximumConnections);
+            connection.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+            connection.addDataSourceProperty("serverName", this.host);
+            connection.addDataSourceProperty("port", this.port);
+            connection.addDataSourceProperty("databaseName", this.database);
+            connection.addDataSourceProperty("user", this.username);
+            connection.addDataSourceProperty("password", this.password);
+
+            this.hikariDataSource = connection;
+        } catch (IllegalStateException e) {
+            throw new ConnectionAlreadyOpenException();
+        } catch (IllegalArgumentException e) {
+            throw new InvalidConnectionCredentialsException();
+        }
+    }
+
+    @Override
+    public void closeConnection() throws ConnectionNotOpenException, UnclosableConnectionException, SQLException {
+        if (this.getConnection() == null) {
+            throw new ConnectionNotOpenException();
+        }
+
+        try {
+            this.hikariDataSource.close();
+        } catch (Exception e) {
+            throw new UnclosableConnectionException();
+        }
+    }
+
+    @Override
+    public boolean connectionOpen() throws SQLException {
+        if (this.getConnection() == null) {
+            return false;
+        }
+
+        return !this.getConnection().isClosed();
+    }
+
+    @Override
+    public java.sql.Connection getConnection() throws SQLException {
+        return this.hikariDataSource.getConnection();
+    }
+}
