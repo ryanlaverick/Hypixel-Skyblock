8159dcbeddf405d9fa5d7a7665c038a0d13532e0
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2024-09-14 00:04:43 +0100

    updates database manager to expose connection as static

--- src/main/java/com/github/ryanlaverick/framework/database/DatabaseManager.java
+++ src/main/java/com/github/ryanlaverick/framework/database/DatabaseManager.java
@@ -8,16 +8,16 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
     private final Skyblock skyblock;
-    private final Connection connection;
+    private static Connection connection = null;
 
     public DatabaseManager(Skyblock skyblock) {
         this.skyblock = skyblock;
-        this.connection = new HikariConnection("localhost", "3306", "skyblock", "root", "", 10);
+        connection = new HikariConnection("localhost", "3306", "skyblock", "root", "", 10);
     }
 
     public void establishConnection() {
         try {
-            this.connection.openConnection();
+            connection.openConnection();
         } catch (ConnectionAlreadyOpenException e) {
             this.skyblock.getLogger().severe("[DATABASE] Unable to open a new connection to the Database, as one already exists.");
         } catch (InvalidConnectionCredentialsException e) {
@@ -27,7 +27,7 @@ public class DatabaseManager {
         }
     }
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         return connection;
     }
 }
