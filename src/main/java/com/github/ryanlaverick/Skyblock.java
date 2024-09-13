ed3b7d44532dec7f083f4a11f8ab13d55e25c693
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2024-09-13 21:12:39 +0100

    introduces database manager to master class

--- src/main/java/com/github/ryanlaverick/Skyblock.java
+++ src/main/java/com/github/ryanlaverick/Skyblock.java
@@ -1,6 +1,18 @@
 package com.github.ryanlaverick;
 
+import com.github.ryanlaverick.framework.database.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Skyblock extends JavaPlugin {
+    private DatabaseManager databaseManager;
+
+    @Override
+    public void onEnable() {
+        this.databaseManager = new DatabaseManager(this);
+        this.databaseManager.establishConnection();
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
 }
